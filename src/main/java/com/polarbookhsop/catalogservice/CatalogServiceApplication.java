f3250de27678cdcfafec65f27df0025ccdb6e72f
5hyunkwon <kmoshn815@naver.com>
2025-07-17 17:23:04 +0900

    :tada: Initial commit

---
+++ src/main/java/com/polarbookhsop/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.polarbookhsop.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
